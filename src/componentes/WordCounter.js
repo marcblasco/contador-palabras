b8a093658f033f2f8b25e6acf0a47f77cc0e6743
Marc Blasco <marcblasconarro@gmail.com>
2022-09-19 11:50:47 +0200

    trying to read sherlok's file

--- src/componentes/WordCounter.js
+++ src/componentes/WordCounter.js
@@ -1,10 +1,9 @@
-
-function readFile(){
   
-}
-
+const fs= require('fs')
 
 function getWords(){
+  const archivo = fs.readFileSync('sherlok.txt', 'utf-8');
+  console.log(archivo);
   const text = 'Scandal the adventures of Sherlok HOLMES Arthur Conan Doyle Table of contents A in Bohemia The Red-Headed League A Case of Identity The Boscombe Valley Mystery The Five Orange Pips The Man with the Twisted Lip The Adventure of the Blue Carbuncle The Adventure of the Speckled Band The Adventure of the Engineers Thumb The Adventure of the Noble Bachelor The Adventure of the Beryl Coronet The Adventure of the Copper Beeches A SCANDAL IN BOHEMIA Table of contents Chapter 1 Chapter 2 Chapter 3';
   const words = text.replace(/[^a-zA-Z0-9 ]/g, "").split(" ").map( word => word.toLowerCase());
   return words;
@@ -15,7 +14,7 @@ function WordCounter(words){
   const repeated = []
   const sigleWords=[];
   words.forEach(word => {
-    const arrayFilter = words.filter(t => t == word)
+    const arrayFilter = words.filter(t => t === word)
     if (arrayFilter.length >= 0) {
       if (repeated.filter(r => r.word === word).length === 0) 
       {
